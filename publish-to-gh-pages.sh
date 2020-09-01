118c138cb8fdd6ab9a81a5ac35cb1b951b706aac
Olivier Rey <rey.olivier@gmail.com>
2020-09-01 10:05:07 +0200

    adding branch script

---
+++ publish-to-gh-pages.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+clear
+
+if [ $# != 1 ]
+then
+  echo "Usage : publish-to-gh-pages.sh [git comment]"
+  exit 0
+fi
+
+git checkout gh-pages
+cp index.html cover.css ./site
+git commit -a -m "$1"
+git push origin gh-pages
+git checkout master
+
+echo "Done."
+
