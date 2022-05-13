5ce896d255ae473dab98d698b02ff98a6e40b952
orey <rey.olivier@gmail.com>
2022-05-14 00:55:33 +0200

    version pdf imprimable

--- rel.sh
+++ rel.sh
@@ -2,6 +2,7 @@
 clear
 
 cp ./latex/CthulhuDark-fr-OReyJdr11.pdf ./_release/cthulhu-dark-fr-v1-0.pdf
+cp ./latex/CthulhuDark-fr-OReyJdr11-printable.pdf ./_release/cthulhu-dark-fr-print-v1-0.pdf
 
 echo "Done"
 
